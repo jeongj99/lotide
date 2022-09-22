d7567e302484ff8a9b6dafd7d0612a2c22fe296d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-22 01:20:52 +0000

    Implemented the eqObjects function and added test cases

---
+++ eqObjects.js
@@ -0,0 +1,55 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let status = true;
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      status = false;
+    }
+  }
+  return status;
+};
+
+const eqObjects = function(object1, object2) {
+  const keyArray1 = Object.keys(object1);
+  const keyArray2 = Object.keys(object2);
+  if (keyArray1.length !== keyArray2.length) {
+    return false;
+  }
+  for (let key of keyArray1) {
+    if (object1[key] !== object2[key]) {
+      if (Array.isArray(object1[key]) &&
+          Array.isArray(object2[key])) {
+        return eqArrays(object1[key], object2[key]);
+      }
+      return false;
+    }
+  }
+  return true;
+}
+
+// Test Case: Primitives as Values
+const ab = {a: "1", b: "2"};
+const ba = {b: "2", a: "1"};
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+
+// Test Case: Arrays as Values
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] }
+assertEqual(eqObjects(cd, cd2), false);
+
