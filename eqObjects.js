e350059850a720187d026c29200eabbe210c5360
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-22 01:23:14 +0000

    Fixed styling with eslint

--- eqObjects.js
+++ eqObjects.js
@@ -35,7 +35,7 @@ const eqObjects = function(object1, object2) {
     }
   }
   return true;
-}
+};
 
 // Test Case: Primitives as Values
 const ab = {a: "1", b: "2"};
@@ -50,6 +50,5 @@ const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true);
 
-const cd2 = { c: "1", d: ["2", 3, 4] }
+const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
-
