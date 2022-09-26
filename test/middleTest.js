08b24d7dfe7be863f6f4c3e007bd23d151b7a3dd
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-26 23:10:30 +0000

    Refactored so that we use Mocha and Chai for test codes

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,19 +1,26 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-// Test Cases (One or two elements)
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-// Test Cases (Odd number of elements)
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-// Test Cases (Even number of elements)
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
-const array = [1, 2, 3];
-middle(array); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(array, [1, 2, 3]);
+describe("#middle", () => {
+  it("return empty array if there is one element in the input array", () => {
+    const inputArray = [1];
+    assert.deepEqual(middle(inputArray), []);
+  });
+  it("return empty array if there are two elements in the input array", () => {
+    const inputArray = [1, 2];
+    assert.deepEqual(middle(inputArray), []);
+  });
+  it("return [3] when input is [1, 2, 3, 4, 5] (array with odd number of elements", () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    assert.deepEqual(middle(inputArray), [3]);
+  });
+  it("return [2, 3] when input is [1, 2, 3, 4] (array with even number of elements", () => {
+    const inputArray = [1, 2, 3, 4];
+    assert.deepEqual(middle(inputArray), [2, 3]);
+  });
+  it("check that original array is not modified after calling middle", () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    middle(inputArray);
+    assert.deepEqual(inputArray, [1, 2, 3, 4, 5]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,18 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-// Test Case 2: Check the original array is not modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe('#tail', () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const array = ['Hello', 'Lighthouse', 'Labs'];
+    assert.deepEqual(tail(array), ['Lighthouse', 'Labs']);
+  });
+  it("returns undefined for []", () => {
+    const array = [];
+    assert.deepEqual(tail(array), []);
+  });
+  it("check that original array is not modified after calling tail", () => {
+    const array = ['Hello', 'Lighthouse', 'Labs'];
+    tail(array);
+    assert.deepEqual(array, ['Hello', 'Lighthouse', 'Labs']);
+  });
+});
