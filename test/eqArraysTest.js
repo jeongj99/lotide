70715a6a5964a9895d19d68593de3562d88125de
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-26 23:54:00 +0000

    Refactored so that we can use Mocha and Chai for test codes

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,21 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const { expect } = require('chai');
 const eqArrays = require('../eqArrays');
 
-//Test Cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
+describe('#eqArrays', () => {
+  it('Return true if both arrays match', () => {
+    const arrayOne = [1, 2, 3];
+    const arrayTwo = [1, 2, 3];
+    assert.strictEqual(eqArrays(arrayOne, arrayTwo), true);
+  });
+  it('Return false if arrays do notmatch', () => {
+    const arrayOne = [1, 2, 3, '4'];
+    const arrayTwo = [1, 2, 3];
+    assert.strictEqual(eqArrays(arrayOne, arrayTwo), false);
+  });
+  it('Throw an error if one or both arrays are not arrays', () => {
+    const arrayOne = 'hello';
+    const arrayTwo = [1, 2, 3];
+    expect(eqArrays, (arrayOne, arrayTwo)).to.throw('Pass only arrays')
+  });
+});
