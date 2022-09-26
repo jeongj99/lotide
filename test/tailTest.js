cf131c269851f151f0d6405151a827e774a8d744
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-26 17:35:33 +0000

    Imported tail and assertEqual functions and made test codes

---
+++ test/tailTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+// Test Case 2: Check the original array is not modified
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
