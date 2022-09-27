4725c78ae70a48499350d6491fd9e2ed015302c6
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-27 02:20:31 +0000

    Refactored to remove everything except the code and exported it

--- countLetters.js
+++ countLetters.js
@@ -1,12 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(string) {
+  if (typeof string !== 'string') {
+    throw Error('Pass only strings');
+  }
   const results = {};
   let onlyLetters = (/[a-z]/);
   for (let letter of string.toLowerCase()) {
@@ -20,12 +15,4 @@ const countLetters = function(string) {
   return results;
 };
 
-// Test Cases
-const test = countLetters("Lh lab9");
-console.log(test);
-assertEqual(test.l, 2);
-assertEqual(test.h, 1);
-assertEqual(test.a, 1);
-assertEqual(test.b, 1);
-assertEqual(test.c, undefined);
-assertEqual(test["9"], undefined);
+module.exports = countLetters;
