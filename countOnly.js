5b6c1ac22e3a0a8335e69c2cec16ecd7afab9f02
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-27 02:56:08 +0000

    Refactored the code, so it throws if there are invalid inputs, and exported the function

--- countOnly.js
+++ countOnly.js
@@ -1,14 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countOnly = function(allItems, itemsToCount) {
+  if (!Array.isArray(allItems) ||
+    (typeof itemsToCount !== 'object' &&
+      itemsToCount !== 'null' &&
+      !Array.isArray(itemsToCount))) {
+    throw new Error('Invalid inputs');
+  }
   const results = {};
-
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (!results[item]) {
@@ -20,22 +17,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-// Test Code
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
